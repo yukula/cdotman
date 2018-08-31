d51eed5a7c12451bdb1f0abbba4dc75d7d9e740d
Vlad Yukhno <jaffeetv@gmail.com>
2018-08-31 15:57:58 +0200

    Add dot_install(dummy) & TARGETS arg validation

--- Dot.cmake
+++ Dot.cmake
@@ -1,4 +1,4 @@
-option(DOT_DEBUG "Enable debuging traces" OFF)
+option(DOT_DEBUG "Enable debug traces" OFF)
 
 function(dot_add_target _target)
     if(NOT ARGN)
@@ -37,5 +37,38 @@ function(dot_add_target _target)
         message("\ttarget_name: ${_target}")
         message("\targn: ${ARGN}")
         message("\tDOT_TARGET_${_target} = ${dot_target}")
+    endif()
+endfunction()
+
+
+function(dot_install)
+    set(prefix          DOT_INSTALL)
+    set(options         LINK)
+    set(oneValueArgs    DESTINATION)
+    set(multiValueArgs  TARGETS)
+    cmake_parse_arguments(${prefix} ${options} ${oneValueArgs} ${multiValueArgs} ${ARGN})
+
+    foreach(target IN LISTS DOT_INSTALL_TARGETS)
+        if(NOT DEFINED DOT_TARGET_${target})
+            message(SEND_ERROR
+                    "dot_install was called with nonexistent target ${target}")
+            return()
+        endif()
+    endforeach()
+
+    # get_filename_component(dot_install_destination ${DOT_INSTALL_DESTINATION} REALPATH)
+    # if(EXISTS dot_install_destination)
+    #     set(a 1)
+    # else()
+    #     message(WARNING
+    #             "In dot_install DESTINATION: ${dot_install_destination} doen't exist")
+    # endif()
+
+    if(DOT_DEBUG)
+        message("In dot_install:")
+        message("\tDOT_INSTALL_TARGETS: ${DOT_INSTALL_TARGETS}")
+        message("\tDOT_INSTALL_DESTINATION: ${DOT_INSTALL_DESTINATION}")
+        message("\tDOT_INSTALL_LINK: ${DOT_INSTALL_LINK}")
+        message("\t")
     endif()
 endfunction()
